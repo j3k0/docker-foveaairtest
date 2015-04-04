a194fc3ec7819a1fe5fd474e90a743c8f35bb6bd
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-04-04 14:31:43 +0000

    docker entry updated

--- docker-entry.sh
+++ docker-entry.sh
@@ -5,7 +5,7 @@ if test -f "/home/air/bin/$1" ; then
     if [ $1 == adl ]; then
         rm -f flexunit.log
         (nc -l 8765; echo) | tr '\000' '\n' | tee flexunit.log | grep 'status="'&
-        su air -c "Xvfb :0" &
+        su air -c "Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile ./xorg.0.log -config /xorg.conf :0" &
         sleep 2
         export DISPLAY=:0
     fi
@@ -13,7 +13,7 @@ if test -f "/home/air/bin/$1" ; then
     su air -c "/home/air/bin/$*"
 
     if [ $1 == adl ]; then
-        su air -c "killall Xvfb"
+        su air -c "killall Xorg"
         wait
         cat flexunit.log
         grep 'status="failure"' flexunit.log && exit 1 || echo "OK"
