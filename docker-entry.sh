fce022ba565ad7a257c5d82bbe8fce240a8861a2
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-04-05 13:31:25 +0000

    Change DISPLAY and Delay

--- docker-entry.sh
+++ docker-entry.sh
@@ -3,9 +3,9 @@
 if test -f "/home/air/bin/$1" ; then
 
     if [ $1 == adl ]; then
-        su air -c "Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile ./xorg.0.log -config /xorg.conf :0" &
-        sleep 2
-        export DISPLAY=:0
+        su air -c "Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile ./xorg.0.log -config /xorg.conf :83" &
+        sleep 5
+        export DISPLAY=:83
 	export WINEDEBUG=-all
         su air -c "/home/air/bin/$*"
         su air -c "killall Xorg"
