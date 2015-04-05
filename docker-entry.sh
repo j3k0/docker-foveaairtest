132caae7d642a0d7cb15a0a1be919052dba281c9
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-04-05 13:16:06 +0000

    No need for servers anymore

--- docker-entry.sh
+++ docker-entry.sh
@@ -3,22 +3,14 @@
 if test -f "/home/air/bin/$1" ; then
 
     if [ $1 == adl ]; then
-        rm -f flexunit.log
-        (nc -l 8765; echo) | tr '\000' '\n' | tee flexunit.log | grep 'status="'&
         su air -c "Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile ./xorg.0.log -config /xorg.conf :0" &
         sleep 2
         export DISPLAY=:0
 	export WINEDEBUG=-all
-    fi
-
-    su air -c "/home/air/bin/$*"
-
-    if [ $1 == adl ]; then
+        su air -c "/home/air/bin/$*"
         su air -c "killall Xorg"
-        wait
-        cat flexunit.log
-        grep 'status="failure"' flexunit.log && exit 1 || echo "OK"
-        grep 'status="error"' flexunit.log && exit 1 || echo "OK"
+    else
+        su air -c "/home/air/bin/$*"
     fi
 
 else
